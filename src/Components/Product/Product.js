f2e86305fa243e46c7705bc89aa0406d82c77a0a
Sabbir Ahmed <sabbirahmed2013303@gmail.com>
2023-02-11 21:50:54 +0600

    product page design

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+const Product = (props) => {
+    console.log(props.product)
+    const {name, img, seller, price, ratings} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>price: ${price}</p>
+                <p><small>Seller : {seller}</small></p>
+                <p><small>Rating: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
